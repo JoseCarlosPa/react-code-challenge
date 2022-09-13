f46f2288b87d70208ec8bf956a092bdbfab59568
Jose Carlos <josecarlospas1@gmail.com>
2022-09-12 19:14:14 -0500

    all

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {GifExpertApp} from "./GifExpertApp";
+
+function App() {
+  return (
+    <div className="App">
+      <GifExpertApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GifItem.tsx
@@ -0,0 +1,10 @@
+
+export const GifItem = ({ title, url, id }:any) => {
+
+  return (
+    <div className="card">
+        <img src={ url } alt={ title } />
+        <p>{ title }</p>
+    </div>
+  )
+}
